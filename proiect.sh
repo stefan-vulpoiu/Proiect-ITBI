7afe9e7656e70954cd48c6841b2aae8dfdd54087
albertcom06 <albertcom06@gmail.com>
2025-12-21 13:51:56 +0000

    Extragerea liniilor care reprezinta doar fisiere si extragerea liniilor care contin doar informatii despre disc

--- proiect.sh
+++ proiect.sh
@@ -6,3 +6,9 @@ if [ $# -ne 2 ]; then
 fi
 before=$1
 after=$2
+
+awk  'NF==9 {print $0}' "$before" > ls_before.txt
+awk  'NF==9 {print $0}' "$after" > ls_after.txt
+
+grep "/$" "$before" > df_before.txt
+grep "/$" "$after" > df_after.txt
