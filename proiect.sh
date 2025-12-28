f78beabf3d00b226e152e49ea75917875b306a60
stefan-vulpoiu <vulpoiu_stefan@yahoo.com>
2025-12-28 16:28:25 +0200

    Am adaugat comanda care pune in fisierul text modificari_ls.txt modificarile aparute, apoi in if am verificat prin ! -s daca fisierul este gol sau daca nu exista deloc, ceea ce inseamna ca nu a fost nimic diferit si fisierele sunt identice, iar pe ramura de else am afisat ce este nou: comanda grep ^>  cauta liniile care contin, dar care si incep cu caracterul > datorita ancorei folosite ^ si am afisat ce este nou si ce este vechi/sters. De asemenea, am facut o mica modificare in codul anterior, punand grep % in loc de comanda anterioara grep /$

--- proiect.sh
+++ proiect.sh
@@ -6,3 +6,20 @@ if [ $# -ne 2 ]; then
 fi
 before=$1
 after=$2
+
+awk  'NF==9 {print $0}' "$before" > ls_before.txt
+awk  'NF==9 {print $0}' "$after" > ls_after.txt
+
+grep "%" "$before" > df_before.txt
+grep "%" "$after" > df_after.txt
+
+diff ls_before.txt ls_after.txt > modificari_ls.txt
+
+if [ ! -s modificari_ls.txt ]; then
+	echo "nu sunt modificari; fisierele sunt identice"
+else
+	echo "nou aparut:"
+	grep "^> " modificari_ls.txt
+	echo "linii sterse:"
+	grep "^< " modificari_ls.txt
+fi
