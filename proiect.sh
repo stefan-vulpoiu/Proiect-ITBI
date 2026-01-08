2955f4b6231ea4a2a426f331642586cf165524b3
albertcom06 <albertcom06@gmail.com>
2026-01-08 16:52:39 +0000

    Am adaugat comanda care pune in fisierul text modificari_df.txt modificarile aparute (comanda diff evalueaza diferentele dintre cele 2 fisiere, -y separa continutul pe 2 coloane, ceea ce era initial <before> si ceea ce a aparut dupa <after> ; prin --suppress-common-lines, codul nu mai afiseaza si liniile de cod comune, ceea ce inseamna ca va fi afisat doar continutul care a suferit modificari ). Apoi in if am verificat prin ! -s daca fisierul este gol sau daca nu exista deloc, ceea ce inseamna ca nu a fost nimic diferit si fisierele sunt identice. Pe ramura de else am afisat modificarile pe 2 coloane liniile modificate. In prima parte a programului am modificat structura pentru a integra si comenzile ls -l si df -h in proiect, acum fara sa mai fie necesar sa le scriem in terminal pentru a rula programul. Dupa ce incepe executarea programului, mai este nevoie doar de comanda de touch in terminal, iar apoi modificarile sunt afisate.

--- proiect.sh
+++ proiect.sh
@@ -1,14 +1,41 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]; then
-	echo "avem nevoie de 2 fisiere pentru a putea evalua situatia de dinainte si de dupa modificarile efectuate"
-	exit 1
-fi
-before=$1
-after=$2
+before="stare_initiala.txt"
+after="stare_finala.txt"
+
+echo "starea initiala:"
+ls -l > "$before"
+df -h >> "$before"
+
+echo  "starea initiala a fost salvata in $before"
+read -p "Apasa tasta ENTER dupa ce ai terminat modificarile pentru a continua."
+
+echo "starea finala:"
+ls -l > "$after"
+df -h >> "$after"
 
 awk  'NF==9 {print $0}' "$before" > ls_before.txt
 awk  'NF==9 {print $0}' "$after" > ls_after.txt
 
-grep "/$" "$before" > df_before.txt
-grep "/$" "$after" > df_after.txt
+grep "%" "$before" > df_before.txt
+grep "%" "$after" > df_after.txt
+
+diff  ls_before.txt ls_after.txt > modificari_ls.txt
+
+if [ ! -s modificari_ls.txt ]; then
+echo "nu sunt modificari; fisierele sunt identice"
+else
+echo "nou aparut:"
+grep "^> " modificari_ls.txt
+echo "linii sterse:"
+grep "^< " modificari_ls.txt
+fi
+
+diff -y --suppress-common-lines df_before.txt df_after.txt > modificari_df.txt
+
+if [ ! -s modificari_df.txt ]; then
+ echo "nicio modificare"
+else 
+ echo "vechi  |  nou"
+ cat modificari_df.txt
+fi
